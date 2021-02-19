40f2abc1b7b126295c18cb864843a757d009ccd4
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-02-19 17:29:15 +0000

    make sure we are always going from root

--- json_tagger/dataset_json.py
+++ json_tagger/dataset_json.py
@@ -49,6 +49,7 @@ class DatasetJSONMappings:
             if not path_root:
                 raise('No JSON files or directory supplied. Set JSON_TAGGER_ROOT to provide a root for the json files.')
 
+            path_root = os.path.abspath(path_root)
             # Generate a list of all JSON files
             json_files = Path('/').glob(os.path.join(path_root.lstrip('/'), '**/*.json'))
 
